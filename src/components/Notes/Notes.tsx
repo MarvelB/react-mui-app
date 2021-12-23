c146326ced016bf65c0b7d4dc49886c2d9858198
bryan.maravilla <bryan.maravilla@ticktradesystems.com>
2021-12-23 15:29:31 -0500

    st l10 commit

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import { Note } from "../../types/note.type";
+import { Container } from "@mui/material";
 
 const Notes = () => {
   const [notes, setNotes] = useState<Note[]>([]);
@@ -14,11 +17,30 @@ const Notes = () => {
   }, []);
 
   return (
-    <div>
-      { notes.map(note => (
-        <p key={ note.id }>{ note.title }</p>
-      )) }
-    </div>
+    <Container>
+      {/* <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container>
+        { notes.map(note => (
+          <Grid item xs={12} md={6} lg={4} key={ note.id }>
+            <Paper>{ note.title }</Paper>
+          </Grid>
+        )) }
+      </Grid>
+    </Container>
   )
 }
 
